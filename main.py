4b462bf186f9e56300cb629fa3b468cc3703c7c5
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-01 17:53:11 -0600

    Part 1
    main work

--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
 def parse_month(month):
-    pass
+    dictionary= {'January': '01' ,'Feburary' : '02' ,'March': '03' ,'April': '04','May': '05', 'June': '06' ,'July': '07','August': '08' ,'September' :'09','October': '10', 'November': '11', 'December': '12'}
+    return dictionary[month]
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    pass
+     split=user_string.split()
 
+    # print(##dic return'/'##date'/'##year)
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
     pass
