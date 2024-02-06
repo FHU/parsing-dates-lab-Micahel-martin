3a482cc5001c52b0e36cedc61061a4ca3d54d17e
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-06 11:46:29 -0600

    Dictionary Updates

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ def parse_month(month):
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
      split=user_string.split()
-
-    # print(##dic return'/'##date'/'##year)
+     list=split
+     print(list[0],'/' list[1],'/' list[2])
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+   def parse_month(month):
+    dictionary= {'January': '01' ,'Feburary' : '02' ,'March': '03' ,'April': '04','May': '05', 'June': '06' ,'July': '07','August': '08' ,'September' :'09','October': '10', 'November': '11', 'December': '12'}
+    return dictionary[month]
+
+#REMOVE PASS AND FIX THIS FUNCTION
+#parse_date function should return the date formated as MM/DD/YYYY
+#DO NOT REMOVE THIS FUNCTION
+def parse_date(user_string):
+     split=user_string.split()
+     list=split
+     print(list[0],'/'list[1],'/'list[2])
